d941b6799a1b11ea867fc2fe2264c935dcc3e4c2
GrammySpaghetti <ryanchuray@me.com>
2019-02-28 11:29:21 -0600

    first assignment

---
+++ assignments/first_program/FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram{
+  public static void main(String[] args){
+    System.out.println("My name is Ryan");
+    System.out.println("My age is none of your business");
+    System.out.println("My hometown is Houston");
+  }
+}
